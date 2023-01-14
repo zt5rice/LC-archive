6a9304bd563b4ecf7bca455c27f4788407998256
Zhao Tang <zhao.tang@rice.edu>
2023-01-13 21:29:55 -0600

    101

--- 0101. Symmetric Tree/Solution0101.cpp
+++ 0101. Symmetric Tree/Solution0101.cpp
@@ -1,4 +1,24 @@
-class Solution0101 {
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        stack<TreeNode*> stk;
+        stk.push(root);
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode* r1 = stk.top(); stk.pop();
+            TreeNode* r2 = stk.top(); stk.pop();
+            if (r1 == nullptr && r2 == nullptr) continue;
+            if (r1 == nullptr || r2 == nullptr || r1->val != r2->val) return false;
+            stk.push(r1->left);
+            stk.push(r2->right);
+            stk.push(r1->right);
+            stk.push(r2->left);
+        }
+        return true;
+    }
+};
+
+class Solution0101_recursion {
 public:
     bool isSymmetric(TreeNode* root) {
         return isSym(root, root);
