ae5914c3a5b1b649e870fecd17516554049df5f0
Zhao Tang <zhao.tang@rice.edu>
2023-01-29 19:08:43 -0600

    1137

--- 1137. N-th Tribonacci Number/Solution1137.java
+++ 1137. N-th Tribonacci Number/Solution1137.java
@@ -1,30 +1,38 @@
 class Solution1137 {
-    public:
-        int tribonacci(int n) {
-            deque<int> dq;
-            int sum = 0;
-            if (n >= 0) {
-                dq.push_back(0);
-                sum += 0;
-                if (n == 0) return 0;
-            }
-            if (n >= 1) {
-                dq.push_back(1);
-                sum += 1;
-                if (n == 1) return 1;
-            }
-            if (n >= 2) {
-                dq.push_back(1);
-                sum += 1;
-                if (n == 2) return 1;
-            }
-            for (int i = 3; i < n; i++) {
-                dq.push_back(sum);
-                int tmp = dq.front(); dq.pop_front();
-                sum -= tmp;
-                sum += dq.back();
-            }
-            return sum;
+    public int tribonacci(int n) {
+        int[] dp = new int[]{0, 1, 1};
+        int cursum = 2;
+        for (int i = 3; i <= n; i++) {
+            int prev = dp[(i-3)%3];
+            dp[i%3] = cursum;
+            cursum = 2*cursum-prev;
         }
-    };
-    // deque ref: http://c.biancheng.net/view/6860.html
+        return dp[n%3];
+    }
+    // public int tribonacci(int n) {
+    //     Deque<Integer> prev = new ArrayDeque<>();
+    //     int sum = 0;
+    //     if (n >= 0) {
+    //         sum += 0;
+    //         prev.offerLast(0);
+    //         if (n == 0) return 0;
+    //     } 
+    //     if (n >= 1) {
+    //         sum += 1;
+    //         prev.offerLast(1);
+    //         if (n == 1) return 1;
+    //     }
+    //     if (n >= 2) {
+    //         sum += 1;
+    //         prev.offerLast(1);
+    //         if (n == 2) return 1;            
+    //     }
+    //     for (int i = 3; i < n; i++) {
+    //         prev.offerLast(sum);
+    //         int tmp = prev.pollFirst(); 
+    //         sum -=  tmp;
+    //         sum += prev.peekLast();  
+    //     }
+    //     return sum;
+    // }
+}
